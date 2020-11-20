fae5def43cabef199e0077d763cb650abb8dcf87
philipk8 <philipk876@gmail.com>
2020-11-20 12:04:58 -0800

    completed phase 1 of contiguous sum

--- exec_time.rb
+++ exec_time.rb
@@ -1,6 +1,6 @@
 #Phase I
 
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 
 def my_min(list) 
 
@@ -13,7 +13,7 @@ def my_min(list)
 
 end
 
-p my_min(list)
+# p my_min(list)
 
 #Phase2
 def my_min2(list)
@@ -29,7 +29,58 @@ def my_min2(list)
     #time complexity is O(n)
 end 
 
-p my_min2(list)
+# p my_min2(list)
 
 #-------------------
 
+def largest_contiguous_subsum(list)
+
+    res = []
+
+    sub_sets = sub_set(list) # operation is n^3 => n^2 long
+
+    sub_sets.each do |sub_arr| # n^2 (number of elements n^2)
+        res << sub_arr.sum # * n => n^3
+    end # n^3
+
+    res.max # + n 
+
+    # n^3 + n^3 + n ==> n^3 
+end 
+
+def sub_set(arr)
+    
+    # return [[]] if arr.empty?
+    # subs = sub_set(arr.take(arr.count - 1))
+    # subs.concat(subs.map { |sub| sub + [arr.last] })
+
+    res = []
+
+    arr.each_with_index do |el1, i1| # n 
+        arr.each_with_index do |el2, i2| # n
+            if i2 >= i1 
+                res << arr[i1..i2] #  * n 
+            end
+        end
+    end
+
+    res # n^3
+end
+
+list1 = [5, 3, -7]
+list2 = [2, 3, -6, 7, -6, 7]
+list3 = [-5, -1, -3]
+
+# p sub_set(list)
+# p sub_set(list2)
+# p sub_set(list3)
+
+# p largest_contiguous_subsum(list1)
+# p largest_contiguous_subsum(list2)
+# p largest_contiguous_subsum(list3)
+
+def largest_contiguous_subsum2(list)
+
+    (0...list.length)
+
+end
